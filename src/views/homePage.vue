b9703c327d3bd47383af4a03d49b270eda5bdc09
alisaza <alizabihi1997@gmail.com>
2022-11-20 02:17:42 +0330

    fix(script): get article list

--- src/views/homePage.vue
+++ src/views/homePage.vue
@@ -1,17 +1,56 @@
 <template>
-  <div>
-    <button @click="test">test</button>
-    <h4>sss</h4>
-  </div>
+  <v-container>
+    <v-overlay :opacity="1" :value="overlay">
+      <v-progress-circular indeterminate size="64"> Loading... </v-progress-circular>
+    </v-overlay>
+    <v-pagination v-model="pageNumber" @input="fetchArticles" :length="totalPageNumber" :total-visible="7" circle></v-pagination>
+  </v-container>
 </template>
 
 <script>
 export default {
   name: "homePage",
+  data: () => ({
+    overlay: true,
+    article: [],
+    pageNumber: 1,
+    totalPageNumber: 1,
+    tags: [],
+  }),
+  computed: {},
+  mounted() {
+    this.fetchArticles();
+    // this.fetchTags();
+  },
   methods: {
-    test() {
-      console.log("sss", this.$route);
+    async fetchArticles() {
+      try {
+        this.overlay = true;
+        const response = await fetch("https://api.realworld.io/api/articles?limit=10" + "&offset=" + (this.pageNumber - 1) * 10, {
+          method: "GET",
+          // authorization: "Token " + this.userToken,
+          headers: {
+            "content-type": "application/json",
+          },
+        });
+        console.log("response***:", response);
+        const responseData = await response.json();
+        console.log("responseData:", responseData);
+        this.totalPageNumber = Math.ceil(responseData.articlesCount / 10);
+        this.article = responseData.articles;
+        console.log("this.totalPageNumber:", this.totalPageNumber);
+      } catch (e) {
+        console.log("error:", e);
+      } finally {
+        this.overlay = false;
+      }
     },
+  },
+  watch: {
+    // pageNumber: function (val) {
+    //   console.log("val:", val);
+    //   this.fetchArticles();
+    // },
   },
 };
 </script>
